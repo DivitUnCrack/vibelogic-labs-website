044e0f654777ea860672c466548d62fcc7a3fa95
DivitUnCrack <mishradivit7@gmail.com>
2026-01-14 10:21:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "./", 
+    base: "./",
 
     server: {
       port: 3000,
@@ -13,11 +13,6 @@ export default defineConfig(({ mode }) => {
     },
 
     plugins: [react()],
-
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
 
     resolve: {
       alias: {
